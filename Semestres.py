4679f26b3776b04dcc6000eb461031fe5eb8a844
rangelnunes <rangelnunes@gmail.com>
2022-06-13 15:04:29 -0300

    [2] - inicio da implementacao da tela de semestres

---
+++ Semestres.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+from tkinter import ttk
+from database import Database
+
+class Semestres(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        # estabelecendo a conexao com o banco de dados
+        self.conexao = Database()
+
+        # coloquem um titulo: gerenciamento de semestres
+        self.title('Gerenciamento de semestres')
+
+        # nao deixem maximizar a janela
+        self.resizable(False, False)
+
+        # definição do estilo da janela
+        self.estilo = ttk.Style(self)
+        self.estilo.theme_use('clam')
+
+    def cria_widgets(self):
+        # criar um frame de nome input_frame
+        self.input_frame = ttk.Frame(self, padding=(20, 10, 20, 0))
+        self.input_frame.grid(row=0, column=0)
+
+        # criando um Label
+        self.label_ano = ttk.Label(self.input_frame, text='Ano: ')
+        self.label_ano.grid(row=0, column=0, sticky=tk.W)
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import tkinter as tk
 from tkinter import ttk
+from Semestres import Semestres
 
 class App(tk.Tk):
     def __init__(self):
         super(App, self).__init__()
         self.title("App da turma de 2022")
+        self.configure(bg='#FFFEE1')
+        self.resizable(False,False)
+        self.eval('tk::PlaceWindow . center')
+
+        self.estilo = ttk.Style(self)
+        self.estilo.theme_use('clam')
+
         self.cria_widgets()
 
     def cria_widgets(self):
@@ -12,10 +20,21 @@ class App(tk.Tk):
         barra_de_ferramentas = tk.Frame(self, bg='#FFFEE1', padx=20, pady=20)
         barra_de_ferramentas.grid(row=0, column=0)
 
+        # definindo estilo do botao
+        self.estilo.configure('TButton', font=('arial', 12), foreground='#3D3D3D')
+
+        # imagens para os botoes
+        icone_semestre = tk.PhotoImage(file='icones/icone-semestre.png')
         # criar um botao
-        botao_cadastrar_semestres = ttk.Button(barra_de_ferramentas, text='Gerenciar semestres')
+        botao_cadastrar_semestres = ttk.Button(barra_de_ferramentas, text='Gerenciar semestres'
+                                               ,image=icone_semestre, compound=tk.TOP, style='TButton',
+                                               command=self.cadastrar_semestres)
+        botao_cadastrar_semestres.image = icone_semestre
         botao_cadastrar_semestres.grid(row=0, column=0, padx=5, sticky=tk.W)
 
+    def cadastrar_semestres(self):
+        window = Semestres(self)
+        window.grab_set()
 
 app = App()
 app.mainloop()
